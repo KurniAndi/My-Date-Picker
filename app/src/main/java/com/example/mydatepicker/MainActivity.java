80959145ef0872f789bc142e87bcb98872097f9a
KurniAndi <andikurniawan111@gmail.com>
2021-02-02 19:02:33 +0700

    initial commit

---
+++ app/src/main/java/com/example/mydatepicker/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mydatepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText TxTanggal;
+    DatePickerDialog datePickerDialog;
+    SimpleDateFormat dateFormatter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TxTanggal = (EditText)findViewById(R.id.txTanggal);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+
+        TxTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
+    }
+
+    private void showDateDialog() {
+        Calendar calendar = Calendar.getInstance();
+
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, month, dayOfMonth);
+                TxTanggal.setText(dateFormatter.format(newDate.getTime()));
+            }
+        },calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.show();
+    }
+}
